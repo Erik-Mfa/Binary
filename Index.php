cb17222d6dbd545466dacae7157d984659f11ea0
erikmfa <erikmfa190230@gmail.com>
2022-04-15 23:30:32 -0300

    database and images

--- Index.php
+++ Index.php
@@ -1,3 +1,4 @@
+<?php include_once "database/wearing.php"?>
 <!DOCTYPE html>
 <html lang="pr-br">
 <head>
@@ -88,6 +89,25 @@
 </ul>
 </div>
 </div>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <img class="img-thumbnail img-fluid" src="<?php echo "./imagens/".$imagem[1].".jpg"?>" width="250" height="200">
+      <img class="img-thumbnail img-fluid" src="<?php echo "./imagens/".$imagem[1].".jpg"?>" width="250" height="200">
+    </div>
+    <div class="col">
+      <img class="img-thumbnail img-fluid" src="<?php echo "./imagens/".$imagem[1].".jpg"?>" width="250" height="200">
+      <img class="img-thumbnail img-fluid" src="<?php echo "./imagens/".$imagem[1].".jpg"?>" width="250" height="200">
+    </div>
+    <div class="col">
+      <img class="img-thumbnail img-fluid" src="<?php echo "./imagens/".$imagem[1].".jpg"?>" width="250" height="200">
+      <img class="img-thumbnail img-fluid" src="<?php echo "./imagens/".$imagem[1].".jpg"?>" width="250" height="200">
+    </div>
+  </div>
+</div>
+
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
